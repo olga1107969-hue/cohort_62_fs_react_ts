4fe0072dc704a9c6fe3466ff253c527f221f8297
Olga <olga1107969@gmail.com>
2026-01-16 01:02:25 +0200

    Update App.tsx for Homework 10

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Lesson_06 from "lessons/Lesson_06/Lesson_06";
 import Lesson_07 from "lessons/Lesson_07/Lesson_07";
 import Lesson_08 from "lessons/Lesson_08/Lesson_08";
 import Lesson_09 from "lessons/Lesson_09/Lesson_09";
+import Lesson_10 from "lessons/Lesson_10/Lesson_10";
 
 // Homeworks
 import Homework_06 from "homeworks/Homework_06/Homework_06";
 import Homework_07 from "homeworks/Homework_07/Homework_07";
 import Homework_08 from "homeworks/Homework_08/Homework_08";
 import Homework_09 from "homeworks/Homework_09/Homework_09";
+import Homework_10 from "homeworks/Homework_10/Homework_10";
 
 function App() {
   return (
@@ -22,8 +24,10 @@ function App() {
       <GlobalStyles />
       {/* <Lesson_08 /> */}
       {/* <Homework_08 /> */}
-      {/*<Lesson_09 />*/}
-      <Homework_09 />
+      {/* <Lesson_09 /> */}
+      {/* <Homework_09 /> */}
+      {/* <Lesson_10 /> */}
+      <Homework_10 />
     </>
   );
 }
